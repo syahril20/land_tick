cd6884077895bdbfe3164ff5434c6c4be3b69fd9
Syahril Ramadhan <rodyulo20@gmail.com>
2023-06-14 12:58:25 +0700

    Update

--- main.go
+++ main.go
@@ -10,11 +10,17 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	godotenv.Load()
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PATCH, echo.DELETE},
+		AllowHeaders: []string{"X-Requested-With", "Content-Type", "Authorization"},
+	}))
 
 	postgres.DatabaseConnection()
 	database.RunMigration()
