d8c0f1e6fce0957d830e4cc168553e7ffa84c7e0
Syahril Ramadhan <rodyulo20@gmail.com>
2023-06-14 13:44:50 +0700

    Update user.go

--- models/user.go
+++ models/user.go
@@ -4,13 +4,13 @@ import "time"
 
 type User struct {
 	Id           int          `json:"id_user" form:"id_user" gorm:"primary_key:auto_increment"`
-	NamaLengkap  string       `json:"nama_lengkap" form:"nama_lengkap" gorm:"type: varchar(30)"`
-	Username     string       `json:"username" form:"username" gorm:"type: varchar(20)"`
-	Email        string       `json:"email" form:"email" gorm:"type: varchar(50)"`
-	Password     string       `json:"password" form:"password" gorm:"type: varchar(20)"`
-	JenisKelamin string       `json:"jenis_kelamin" form:"jenis_kelamin" gorm:"type: varchar(10)"`
-	Telp         string       `json:"telp" form:"telp" gorm:"type: varchar(15)"`
-	Alamat       string       `json:"alamat" form:"alamat" gorm:"type: varchar(50)"`
+	NamaLengkap  string       `json:"nama_lengkap" form:"nama_lengkap" gorm:"type: varchar(100)"`
+	Username     string       `json:"username" form:"username" gorm:"type: varchar(100)"`
+	Email        string       `json:"email" form:"email" gorm:"type: varchar(100)"`
+	Password     string       `json:"password" form:"password" gorm:"type: varchar(100)"`
+	JenisKelamin string       `json:"jenis_kelamin" form:"jenis_kelamin" gorm:"type: varchar(100)"`
+	Telp         string       `json:"telp" form:"telp" gorm:"type: varchar(100)"`
+	Alamat       string       `json:"alamat" form:"alamat" gorm:"type: varchar(100)"`
 	IdRole       int          `json:"id_role" form:"id_role"`
 	RoleName     RoleResponse `json:"role_name" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:IdRole"`
 	CreatedAt    time.Time    `json:"-"`
